47bd6356ce3c4c180122c199b11174dd5e15f23e
jguthrie <jguthrie@brokersys.com>
2010-03-11 10:23:37 -0600

    Eliminate a warning when compiled with all warnings.
    Ignore-this: 5bde80a7a4bccdfeeaca157323d03402
    
    darcs-hash:20100311162337-1210e-fc570dd23c7937cb56339a7f8bd7d9cd109db83e

--- deltaqueueaction.cpp
+++ deltaqueueaction.cpp
@@ -2,7 +2,7 @@
 
 #include "deltaqueueaction.hpp"
 
-DeltaQueueAction::DeltaQueueAction(int delta, SessionDriver *driver) : m_delta(delta), m_driver(driver), next(NULL)
+DeltaQueueAction::DeltaQueueAction(int delta, SessionDriver *driver) : next(NULL), m_delta(delta), m_driver(driver)
 {
 }
 
