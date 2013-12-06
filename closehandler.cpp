ccb8e2f924566a894bac5f946d531e3a1e7fbc17
Jonathan Guthrie <jguthrie@brokersys.com>
2013-12-05 18:34:15 -0600

    Reformat closehandler.cpp so I don't combine a reformat with the changes required to eliminate all warnings with -Wall.

--- closehandler.cpp
+++ closehandler.cpp
@@ -17,25 +17,25 @@
 #include "closehandler.hpp"
 
 ImapHandler *closeHandler(ImapSession *session, INPUT_DATA_STRUCT &input) {
-  (void) input;
-  return new CloseHandler(session);
+    (void) input;
+    return new CloseHandler(session);
 }
 
 IMAP_RESULTS CloseHandler::receiveData(INPUT_DATA_STRUCT &input) {
-  // If the mailbox is open, close it
-  // In IMAP, deleted messages are always purged before a close
-  IMAP_RESULTS result = IMAP_TRY_AGAIN;
-  if (MailStore::SUCCESS == m_session->store()->lock()) {
-    NUMBER_SET purgedMessages;
-    m_session->store()->listDeletedMessages(&purgedMessages);
-    m_session->purgedMessages(purgedMessages);
-    for(NUMBER_SET::iterator i=purgedMessages.begin(); i!=purgedMessages.end(); ++i) {
-      m_session->store()->expungeThisUid(*i);
+    // If the mailbox is open, close it
+    // In IMAP, deleted messages are always purged before a close
+    IMAP_RESULTS result = IMAP_TRY_AGAIN;
+    if (MailStore::SUCCESS == m_session->store()->lock()) {
+	NUMBER_SET purgedMessages;
+	m_session->store()->listDeletedMessages(&purgedMessages);
+	m_session->purgedMessages(purgedMessages);
+	for(NUMBER_SET::iterator i=purgedMessages.begin(); i!=purgedMessages.end(); ++i) {
+	    m_session->store()->expungeThisUid(*i);
+	}
+	m_session->store()->mailboxClose();
+	m_session->state(ImapAuthenticated);
+	m_session->store()->unlock();
+	result = IMAP_OK;
     }
-    m_session->store()->mailboxClose();
-    m_session->state(ImapAuthenticated);
-    m_session->store()->unlock();
-    result = IMAP_OK;
-  }
-  return result;
+    return result;
 }
